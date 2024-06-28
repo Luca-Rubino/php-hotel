5485f43e2b7819f44923a71fa7d3aeeaa1e18fd3
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-28 09:44:37 +0200

    Creare form per filtrare per la presenza parcheggi

--- index.php
+++ index.php
@@ -15,32 +15,46 @@ require __DIR__ . "/listaHotel/hotel.php";
     <title>php-hotel</title>
 </head>
 <body>
-    
 
-<table>
-    <thead>
-        <tr>
-        <th scope="col">Hotel</th>
-        <th scope="col">Descrizione</th>
-        <th scope="col">Parcheggio</th>
-        <th scope="col">voto</th>
-        <th scope="col">Distanza dal centro</th>
-        </tr>
-    </thead>
-    <tbody class="table-group-divider">
-        <?php foreach ($hotels as $key => $hotel) { ?>
-        <tr>
-        <th><?php echo $hotel['name'] ?></th>
-        <td><?php echo $hotel['description'] ?></td>
-        <td>
-        <?php echo $hotel['parking'] ? 'Yes' : 'No' ?>
-        </td>
-        <td><?php echo $hotel['vote'] ?></td>
-        <td><?php echo $hotel['distance_to_center'] ?>.km</td>
-        </tr>
-        <?php } ?>
-    </tbody>
-</table>
+    <section>
+
+        <form action="" method="get">
+            <label for="parcheggi">Presenza parcheggi: </label>
+            <select name="parcheggi" id="parcheggi">
+                <option value="yes">Si</option>
+                <option value="no">No</option>
+            </select>
+            <input type="submit" value="Invia">
+        </form>
+        
+    </section>
+
+    <section>
+        <table>
+            <thead>
+                <tr>
+                <th scope="col">Hotel</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">voto</th>
+                <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $key => $hotel) { ?>
+                <tr>
+                <th><?php echo $hotel['name'] ?></th>
+                <td><?php echo $hotel['description'] ?></td>
+                <td>
+                <?php echo $hotel['parking'] ? 'Yes' : 'No' ?>
+                </td>
+                <td><?php echo $hotel['vote'] ?></td>
+                <td><?php echo $hotel['distance_to_center'] ?>.km</td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </section>
 
 </body>
 </html>
